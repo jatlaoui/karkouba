7a732212adbcd9550462102953391a69e4033816
jatlaoui <jatlaoui2016@gmail.com>
2025-05-31 21:48:05 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 8080,
+    host: true, // Automatic host detection
+    strictPort: false,
+    cors: true,
+    open: false,
+    hmr: true,
+    middlewareMode: false,
+    force: true
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  preview: {
+    port: 8080,
+    host: true
+  }
+})
+
